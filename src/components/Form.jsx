a71a0a92a2f963e45c891f97adf414ea6974f710
Sumit Agarwal <97662774+sumit334@users.noreply.github.com>
2023-05-27 07:22:55 +0000

    Snack Bar added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,73 @@
-import {Box,InputBase,Button,styled} from '@mui/material'
+// import {Box,InputBase,Button,styled} from '@mui/material'
+// import {useState} from 'react'
+// import {getWeather} from '../services/api';
+
+// const Container=styled(Box)({
+//     background:'#445A6F',
+//     padding:'10px'
+// });
+
+// const Input=styled(InputBase)({
+//     color:'#FFF',
+//     marginRight:'20px',
+//     fontSize:'18px'
+// });
+
+// const GetButton=styled(Button)({
+//     background:'#e67e22',
+// })
+
+
+// const Form=({setResult})=>{
+//     const [data,setData]=useState({city:'',country:''})
+
+//     const handleChange=(e)=>{
+//         setData({...data ,[e.target.name]:e.target.value})
+//         //console.log(data);
+//     }
+
+//     const getWeatherInfo= async ()=>{
+//         let response = await getWeather(data.city,data.country);
+//         setResult(response);
+//         //console.log(response);
+//     }
+
+//     return (
+//         <Container>
+//             <Input
+//                 placeholder='City'
+//                 onChange={(e)=>handleChange(e)}
+//                 name='city'
+//             />
+//             <Input
+//                 placeholder='Country'
+//                 onChange={(e)=>handleChange(e)}
+//                 name='country'
+//             />
+//             <GetButton
+//                 variant='contained'
+//                 onClick={()=>getWeatherInfo()}
+//             >Get Weather</GetButton>
+//         </Container>
+//     )
+// }
+
+// export default Form;
+//default export should be only one in one file
+//otherwise we need to use name export
+
+//for More Responsive view:
+
+
+import {Grid,InputBase,Button,styled} from '@mui/material'
 import {useState} from 'react'
 import {getWeather} from '../services/api';
 
-const Container=styled(Box)({
-    background:'#445A6F',
-    padding:'10px'
-});
+
+// const Container=styled(Box)({
+//     background:'#445A6F',
+//     padding:'10px'
+// });
 
 const Input=styled(InputBase)({
     color:'#FFF',
@@ -18,39 +80,54 @@ const GetButton=styled(Button)({
 })
 
 
-const Form=({setResult})=>{
-    const [data,setData]=useState({city:'',country:''})
-
-    const handleChange=(e)=>{
-        setData({...data ,[e.target.name]:e.target.value})
-        //console.log(data);
+const Form = ({ setResult }) => {
+    const [data, setData] = useState({ city: '', country: '' })
+  
+    const handleChange = (e) => {
+      setData({ ...data, [e.target.name]: e.target.value })
     }
-
-    const getWeatherInfo= async ()=>{
-        let response = await getWeather(data.city,data.country);
-        setResult(response);
-        //console.log(response);
+  
+    const getWeatherInfo = async () => {
+      let response = await getWeather(data.city, data.country)
+      setResult(response)
     }
-
+  
     return (
-        <Container>
-            <Input
-                placeholder='City'
-                onChange={(e)=>handleChange(e)}
-                name='city'
-            />
-            <Input
-                placeholder='Country'
-                onChange={(e)=>handleChange(e)}
-                name='country'
-            />
-            <GetButton
-                variant='contained'
-                onClick={()=>getWeatherInfo()}
-            >Get Weather</GetButton>
-        </Container>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ bgcolor: '#445A6F', padding: '10px' }}
+      >
+        <Grid item xs={12} sm={4} md={3}>
+          <Input
+            placeholder='City'
+            onChange={(e) => handleChange(e)}
+            name='city'
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={4} md={3}>
+          <Input
+            placeholder='Country'
+            onChange={(e) => handleChange(e)}
+            name='country'
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={4} md={3}>
+          <GetButton
+            variant='contained'
+            onClick={() => getWeatherInfo()}
+            fullWidth
+          >
+            Get Weather
+          </GetButton>
+        </Grid>
+      </Grid>
     )
-}
+  }
 
 export default Form;
 //default export should be only one in one file
--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,54 @@
-
 import axios from 'axios';
+const API_KEY = '675a6f1f0e4160179b80a9177f7c345d';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-const API_KEY='675a6f1f0e4160179b80a9177f7c345d';
-const API_URL='https://api.openweathermap.org/data/2.5/weather';
+export const getWeather = async (city, country) => {
+  try {
+    let response = await axios.get(
+      `${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+    showSnackbar('Got the weather');
+    return response.data;
+  } catch (error) {
+    showSnackbar('Enter the Info');
+    
+    //return error.response;
+  }
+};
 
-export const getWeather= async (city,country)=>{
-    //always whenever you are required to fetch an api then you must use try and catch block to handle the exception so that the call does'nt throws any error
-    try {
-        let response=await axios.get(`${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`);
-        return response.data;
-    } catch (error) {
-        console.log('Error while fetching the weather api',error.message);
-        return error.response;
-    }
-}
+const showSnackbar = (message) => {
+  const snackbar = document.createElement('div');
+  snackbar.textContent = message;
+  if(message==='Got the weather'){
+    snackbar.style.cssText = `
+    position: fixed;
+    bottom: 16px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: green;
+    color: #fff;
+    padding: 16px;
+    border-radius: 4px;
+    font-family: Arial, sans-serif;
+    z-index: 9999;
+  `;
+  }
+  else{
+    snackbar.style.cssText = `
+    position: fixed;
+    bottom: 16px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: orange;
+    color: #fff;
+    padding: 16px;
+    border-radius: 4px;
+    font-family: Arial, sans-serif;
+    z-index: 9999;
+  `;
+  }
+  document.body.appendChild(snackbar);
+  setTimeout(() => {
+    snackbar.remove();
+  }, 3000);
+};
